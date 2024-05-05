bc8d58b41251b0052c2b0e8617338d0f65dd774c
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-05 19:13:37 -0400

    change home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <h1>Animation Practice</h1>
 <p>
-	This is a animation playground build with SvelteKit. This playgournd follow the practice in <a
+	This is an animation playground build with SvelteKit. The playgournd is for my personal practice
+	inspired by <a
 		target="_blank"
 		href="https://www.udemy.com/course/css-animation-transitions-and-transforms-creativity-course"
 		>Creative Advanced CSS & JavaScript Animations - 150 Projects</a
