cb19365d2530109d23ebec1b6979eeaccecda236
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-05 21:45:05 -0400

    capitalize string

--- src/routes/animation/routetag.svelte
+++ src/routes/animation/routetag.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { cn } from '$lib/utils.js';
+	import { cn, capitalizeString } from '$lib/utils.js';
 	export let route;
 </script>
 
 <div class={cn(['text-sm', { 'bg-gray-100 rounded-md': $page.url.pathname === route.path }])}>
-	<a href={route.path}>{route.name}</a>
+	<a href={route.path}>{capitalizeString(route.name)}</a>
 </div>
