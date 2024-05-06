19968830b0be7bd1acaff3b27f3c06d62ec4cede
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-05 22:16:39 -0400

    fix one typo

--- src/routes/animation/+page.svelte
+++ src/routes/animation/+page.svelte
@@ -2,4 +2,4 @@
 </script>
 
 <h1 class="title">Animation</h1>
-<p class="paragraph">Click the nav on the right side to show animation</p>
+<p class="paragraph">Click the nav on the left side to show animation</p>
