873a94397d552535151edf76b2d8dc56cc46425f
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-06 00:52:44 -0400

    chage back text font

--- src/routes/animation/button/[name]/+page.svelte
+++ src/routes/animation/button/[name]/+page.svelte
@@ -10,7 +10,7 @@
 </svelte:head>
 <div class="container">
 	<svelte:component this={data.component} href="/animation/button">Hover Me</svelte:component>
-	<p>Click button to go back</p>
+	<p class="paragraph mt-4">Click button to go back</p>
 </div>
 <Badge class="ml-4">{data.params.name}.svelte</Badge>
 <Script code={data.code} />
@@ -20,11 +20,5 @@
 		display: grid;
 		place-content: center;
 		place-items: center;
-	}
-	p {
-		font-size: 1.5rem;
-		font-weight: 600;
-		margin-bottom: 16px;
-		font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 	}
 </style>
