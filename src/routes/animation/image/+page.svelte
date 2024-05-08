41f03f444ff89e2e3dda08351864d64e8adf6fde
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-08 00:43:33 -0400

    add a image page

---
+++ src/routes/animation/image/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import FadeHoverImage from './fade-hover-image.svelte';
+</script>
+
+<svelte:head>
+	<title>Image Transition</title>
+</svelte:head>
+
+<h1 class="title mb-8">Image Transitions</h1>
+
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+	<div>
+		<FadeHoverImage />
+	</div>
+</div>
---
+++ src/routes/animation/image/fade-hover-image.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import frierenCat1 from '$lib/assets/images/frieren-cat-1.jpeg?enhanced';
+</script>
+
+<figure>
+	<enhanced:img src={frierenCat1} alt="Feren and Stark in cat form, Frieren fan art" />
+	<figcaption>
+		Feren and Stark in cat form, Frieren fan art by <a
+			class="underline"
+			href="https://twitter.com/BoHumbo">JHumbo</a
+		>
+	</figcaption>
+</figure>
