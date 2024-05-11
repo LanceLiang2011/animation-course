08796fc65cf70486e2e03566623e6e017195d602
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-10 21:49:40 -0400

    finish a card

--- src/routes/animation/cards/background-transition-card.svelte
+++ src/routes/animation/cards/background-transition-card.svelte
@@ -1,19 +1,33 @@
 <script lang="ts">
 </script>
 
-<article class="relative rounded-md p-10 shadow-lg">
+<article
+	class="relative overflow-hidden rounded-md p-10 text-center shadow-lg transition-colors duration-500 hover:text-white"
+>
 	<slot name="title" class="mb-3 text-2xl font-bold" />
 	<slot name="content" class="" />
-	<a href="#">See Code</a>
+	<a
+		class=" mt-4 block rounded-sm bg-slate-800 px-3 py-2 font-semibold text-white"
+		target="_blank"
+		href="https://github.com/LanceLiang2011/animation-course/blob/master/src/routes/animation/cards/background-transition-card.svelte"
+		>See Code</a
+	>
 </article>
 
 <style lang="postcss">
-	article :global([slot='title']) {
+	article::before {
+		content: '';
+		@apply absolute bottom-0 left-0 -z-50 h-full w-full origin-bottom scale-y-[2%] rounded-md bg-sky-700 transition-transform duration-500;
+	}
+	article:hover::before {
+		@apply scale-y-100;
+	}
+	/* article :global([slot='title']) {
 	}
 
 	article :global([slot='content']) {
 	}
 
 	a {
-	}
+	} */
 </style>
