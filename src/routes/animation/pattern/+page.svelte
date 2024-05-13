f8277f649fe98c53503529b0a30a5219bf78d088
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-12 23:25:13 -0400

    Allow scroll state change

--- src/routes/animation/pattern/+page.svelte
+++ src/routes/animation/pattern/+page.svelte
@@ -1,7 +1,55 @@
-<div class="h-screen bg-chess-pattern bg-left-top bg-repeat"></div>
+<script lang="ts">
+	import * as Select from '$lib/components/ui/select/index.js';
+	const optionsX = ['Left', 'No', 'Right'];
+	const optionsY = ['Up', 'No', 'Down'];
+	let x: 'Left' | 'No' | 'Right' = 'Right';
+	let y: 'Up' | 'No' | 'Down' = 'Down';
+	$: xNum = x === 'No' ? 0 : x === 'Left' ? -1 : 1;
+	$: yNum = y === 'No' ? 0 : y === 'Up' ? -1 : 1;
+</script>
+
+<div class="mb-8 flex max-w-fit flex-col gap-2">
+	<div>
+		<label for="x" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+			>Horizontal Scroll</label
+		>
+		<select
+			id="x"
+			bind:value={x}
+			class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+		>
+			{#each optionsX as opt}
+				<option value={opt}>
+					{opt}
+				</option>
+			{/each}
+		</select>
+	</div>
+	<div>
+		<label for="y" class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+			>Horizontal Scroll</label
+		>
+		<select
+			id="y"
+			bind:value={y}
+			class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+		>
+			{#each optionsY as opt}
+				<option value={opt}>
+					{opt}
+				</option>
+			{/each}
+		</select>
+	</div>
+</div>
+
+<div
+	class="img h-screen bg-chess-pattern bg-left-top bg-repeat"
+	style="--x:{xNum}; --y:{yNum}"
+></div>
 
 <style>
-	div {
+	div.img {
 		animation: roll-pattern 16s infinite linear;
 	}
 	@keyframes roll-pattern {
@@ -9,7 +57,7 @@
 			background-position: 0 0;
 		}
 		to {
-			background-position: 1280px 1280px;
+			background-position: calc(1280px * var(--x)) calc(1280px * var(--y));
 		}
 	}
 </style>
