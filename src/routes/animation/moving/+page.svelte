585a9e18436defebbd84f59ace8ba7a6c61f8267
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-12 23:05:39 -0400

    add see code buttons

--- src/routes/animation/moving/+page.svelte
+++ src/routes/animation/moving/+page.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import dragonBall from '$lib/assets/images/dragon-ball.gif';
+	import { Button } from '$lib/components/ui/button/index';
 </script>
 
-<div class="bg relative h-[28rem] bg-suburban-street bg-right-bottom">
+<div class="bg relative mb-12 h-[28rem] bg-suburban-street bg-right-bottom">
 	<img
 		src={dragonBall}
 		alt="Running Son Goku"
 		class="absolute bottom-0 left-1/2 -translate-x-1/2"
 	/>
 </div>
+
+<Button
+	target="_blank"
+	href="https://github.com/LanceLiang2011/animation-course/blob/master/src/routes/animation/moving/+page.svelte"
+	>See Code</Button
+>
 
 <style lang="postcss">
 	.bg {
--- src/routes/animation/rain-effect/+page.svelte
+++ src/routes/animation/rain-effect/+page.svelte
@@ -1,11 +1,21 @@
-<div class=" h-96 bg-fa-rain bg-no-repeat">
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index';
+</script>
+
+<div class=" mb-12 h-96 bg-fa-rain bg-no-repeat">
 	<div class="h-full animate-[rain_3s_infinite_linear] bg-rain"></div>
 </div>
+
+<Button
+	target="_blank"
+	href="https://github.com/LanceLiang2011/animation-course/blob/master/src/routes/animation/rain-effect/+page.svelte"
+	>See Code</Button
+>
 
 <style lang="postcss">
 	@keyframes -global-rain {
 		from {
-			background-position: 0% 0%;
+			background-position: 0 0;
 		}
 		to {
 			background-position: 500px 500px;
