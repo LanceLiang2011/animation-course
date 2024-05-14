517fdc369cf8ec9f731045dc9aca7efb705aae3a
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-13 21:45:26 -0400

    update spinner

--- src/routes/animation/loader/irragular-border-loader.svelte
+++ src/routes/animation/loader/irragular-border-loader.svelte
@@ -2,9 +2,9 @@
 
 <div class="relative flex h-40 w-40 items-center justify-center">
 	{#each [0, 1, 2] as _}
-		<span class="absolute inset-0 block animate-spin text-gray-500"></span>
+		<span class="absolute inset-0 block animate-spin"></span>
 	{/each}
-	<p>Loading...</p>
+	<p class="font-mono font-thin text-gray-500">Loading...</p>
 </div>
 
 <style lang="postcss">
@@ -12,15 +12,23 @@
 		border-radius: 30% 70% 71% 29% / 30% 35% 65% 70%;
 		border-width: 0.25rem;
 		border-color: gray;
+		animation: spin 2s linear infinite;
 	}
 	span:nth-child(2) {
 		border-radius: 60% 40% 61% 39% / 47% 75% 25% 53%;
 		border-width: 0.25rem;
 		border-color: gray;
+		animation: spin 1.8s linear reverse infinite;
 	}
 	span:nth-child(3) {
 		border-radius: 37% 63% 29% 71% / 64% 18% 82% 36%;
 		border-width: 0.25rem;
 		border-color: gray;
+		animation: spin 3.1s linear reverse infinite;
+	}
+	@keyframes spin {
+		to {
+			transform: rotate(360deg);
+		}
 	}
 </style>
