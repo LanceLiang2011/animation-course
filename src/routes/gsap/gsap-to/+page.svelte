445349f971c45c485751cf7af08b30752d372cf3
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-15 00:54:39 -0400

    Add a pause toggle

--- src/routes/gsap/gsap-to/+page.svelte
+++ src/routes/gsap/gsap-to/+page.svelte
@@ -2,7 +2,10 @@
 	import type { Action } from 'svelte/action';
 	import Script from '$lib/components/script.svelte';
 	import { gsap } from 'gsap';
+	import { Switch } from '$lib/components/ui/switch';
 	import anya from '$lib/assets/images/Anya_Forger_Anime.png';
+
+	let animate = true;
 
 	interface GsapParams {
 		from: gsap.TweenVars;
@@ -43,31 +46,50 @@
 
 	//...
 	<img
-			class=" min-w-36 origin-bottom"
-			use:gsap_fromto={{
-				from: { x: -200, rotate: -10 },
-				to: { 
-					x: 10, rotate: 10, duration: 2, 
-					repeat: -1, yoyo: true, ease: 'power1.inOut' 
-				}
-			}}
-			src={anya}
-			alt="Anya Forger"
-		/>
+				class=" min-w-36 origin-bottom"
+				use:gsap_fromto={{
+					from: { x: 10, rotate: 10 },
+					to: {
+						x: -100,
+						rotate: -10,
+						duration: 2,
+						repeat: -1,
+						yoyo: true,
+						ease: 'power1.inOut',
+						paused: !animate
+					}
+				}}
+				src={anya}
+				alt="Anya Forger"
+			/>
 	`;
 </script>
 
 <div class=" grid grid-cols-1 md:grid-cols-3">
 	<div class=" col-span-1">
-		<img
-			class=" min-w-36 origin-bottom"
-			use:gsap_fromto={{
-				from: { x: -200, rotate: -10 },
-				to: { x: 10, rotate: 10, duration: 2, repeat: -1, yoyo: true, ease: 'power1.inOut' }
-			}}
-			src={anya}
-			alt="Anya Forger"
-		/>
+		<div>
+			<img
+				class=" min-w-36 origin-bottom"
+				use:gsap_fromto={{
+					from: { x: 10, rotate: 10 },
+					to: {
+						x: -100,
+						rotate: -10,
+						duration: 2,
+						repeat: -1,
+						yoyo: true,
+						ease: 'power1.inOut',
+						paused: !animate
+					}
+				}}
+				src={anya}
+				alt="Anya Forger"
+			/>
+		</div>
+		<div class=" pt-12">
+			<span>Animation: {animate ? 'On' : 'Off'}</span>
+			<Switch bind:checked={animate} />
+		</div>
 	</div>
 	<div class=" col-span-2" use:gsap_fromto={{ from: {}, to: { x: 50, duration: 2 } }}>
 		<Script {code} />
