2d89d4eaec7cadb4f51d0fa0e1be7ae3052c98ed
Xianyi Liang <liangxianyi2011@gmail.com>
2024-05-15 16:33:59 -0400

    update home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,8 @@
 <h1 class="title">Animation Practice</h1>
 <p class="paragraph">
-	This is an animation playground build with SvelteKit. The playgournd is for my personal practice
-	inspired by
+	This is an animation playground build with SvelteKit for my personal practice
 </p>
-<ul class=" my-6 flex flex-col gap-2 text-lg text-blue-800">
-	<li>
-		<a
-			target="_blank"
-			href="https://www.udemy.com/course/css-animation-transitions-and-transforms-creativity-course"
-			>Creative Advanced CSS & JavaScript Animations - 150 Projects</a
-		>
-	</li>
-	<li>
-		<a
-			target="_blank"
-			href="https://www.udemy.com/course/master-javascript-animations-with-greensock"
-		>
-			Master JavaScript Animations with Greensock</a
-		>
-	</li>
-</ul>
+
 <p class="paragraph">You may use Chrome Browser to get a better page transition experience.</p>
 <p class="paragraph">
 	Apologize for not being mobile friendly at this point. Some animations and hover effect make
